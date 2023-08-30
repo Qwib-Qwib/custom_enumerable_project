428be66f4481f85b2695908b76f3c5a97965360c
Julien Pons <jp93rom@orange.fr>
2023-08-30 12:15:57 +0200

    Implement answers for methods up to each_with_index.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,39 @@
 module Enumerable
   # Your code goes here
+  def my_all?
+    for item in self do
+      return false if yield(item) == false
+    end
+    true
+  end
+
+  def my_any?
+    for item in self do
+      return true if yield(item) == true
+    end
+    false
+  end
+
+  def my_count
+    if block_given?
+      count = 0
+      for item in self do
+        count += 1 if yield(item) == true
+      end
+      count
+    else
+      length
+    end
+  end
+
+  def my_each_with_index
+    index = 0
+    for item in self do
+      yield(item, index)
+      index += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +42,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for item in self do
+      yield(item)
+    end
+    self
+  end
 end
