aed31ecc9aeab950c39f6351b6f48f57b99953c3
Julien Pons <jp93rom@orange.fr>
2023-09-03 18:32:47 +0200

    Fix my_each_with_index, remove a few commented-out lines, add a comment to explain why my_each_with_index works as it does.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -3,19 +3,19 @@ module Enumerable
   def my_all?
     # The splat operator makes it so that the  block can be given a variable amount of arguments, making it work for
     # both arrays and hashes without further coding required.
-    my_each { |*elements| return false if yield(*elements) == false }
+    my_each { |*collection_item| return false if yield(*collection_item) == false }
     true
   end
 
   def my_any?
-    my_each { |*elements| return true if yield(*elements) }
+    my_each { |*collection_item| return true if yield(*collection_item) }
     false
   end
 
   def my_count
     if block_given?
       count = 0
-      my_each { |*elements| count += 1 if yield(*elements) }
+      my_each { |*collection_item| count += 1 if yield(*collection_item) }
       count
     else
       length
@@ -24,32 +24,39 @@ module Enumerable
 
   def my_each_with_index
     index = 0
-    my_each do |*elements|
-      yield(*elements, index)
+    # The splat operator wraps the item into an array, which is desirable when iterating over a hash to imitate the
+    # hash behavior of each_with_index, but not when iterating over an array since we want to access each item directly
+    # in that case.
+    # To prevent issues, each item is converted into its content if its length is 1 (i.e. if it's a single object taken
+    # from an array, as opposed to a key-value pair taken from a hash. No super satisfied with that answer, I'm not sure
+    # it's all that extensible.
+    my_each do |*collection_item|
+      collection_item = collection_item[0] if collection_item.length == 1
+      yield(collection_item, index)
       index += 1
     end
     self
   end
 
   def my_inject(initial_value = 0)
-    my_each { |*elements| initial_value = yield(initial_value, *elements) }
+    my_each { |*collection_item| initial_value = yield(initial_value, *collection_item) }
     initial_value
   end
 
   def my_map
     new_array = []
-    my_each { |*elements| new_array.push(yield(*elements)) }
+    my_each { |*collection_item| new_array.push(yield(*collection_item)) }
     new_array
   end
 
   def my_none?
-    my_each { |*elements| return false if yield(*elements) }
+    my_each { |*collection_item| return false if yield(*collection_item) }
     true
   end
 
   def my_select
     selected_items = []
-    my_each { |*elements| selected_items.push(*elements) if yield(*elements) }
+    my_each { |*collection_item| selected_items.push(*collection_item) if yield(*collection_item) }
     selected_items
   end
 end
@@ -62,7 +69,6 @@ class Array
   # Define my_each here
   def my_each(&block)
     for item in self do
-      # yield(item)
       block.call(item)
     end
     self
@@ -72,7 +78,6 @@ end
 class Hash
   def my_each(&block)
     for key, value in self do
-      # yield(key, value)
       block.call(key, value)
     end
     self
